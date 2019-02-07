80d01fe1a338247c7d6a1cc917da77644c215a1f
sanikommugayathri <sanikommugayathri@gmail.com>
2019-02-07 12:53:44 -0600

    Added layouts for welcome activity, league activity, skills activity

--- app/src/main/java/com/example/gayat/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/gayat/swoosh/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package com.example.gayat.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getstarted_button.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
